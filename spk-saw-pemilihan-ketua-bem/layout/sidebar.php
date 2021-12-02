8403be447e50062dc21151dfe245a8ff4f836027
Annisa Syasqia Zain <89344664+asyasqiaz@users.noreply.github.com>
2021-12-02 15:45:11 +0700

    Add files via upload

---
+++ spk-saw-pemilihan-ketua-bem/layout/sidebar.php
@@ -0,0 +1,75 @@
+<div id="sidebar" class="active">
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header">
+        <img src="assets/images/logo.png" style="width:60px;height:120px; margin-bottom:25px; margin-left:90px" />
+            <div class="d-flex justify-content-between">
+                <div class="logo-sidebar">
+                    <a href="./">SPK Pemilihan Ketua BEM</a>
+                </div>
+                <div class="toggler">
+                    <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li class="sidebar-title">Menu</li>
+
+                <li class="sidebar-item">
+                    <a href="indeks.php" class='sidebar-link'>
+                        <i class="bi bi-grid-fill"></i>
+                        <span>Home</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                        <span>Data</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href="alternatif.php">Alternatif</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="kriteria-bobot.php">Kriteria &amp; Bobot</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="subkriteria.php">Sub Kriteria</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="perhitungan.php" class='sidebar-link'>
+                        <i class="bi bi-pentagon-fill"></i>
+                        <span>Perhitungan</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="hasil-perhitungan.php" class='sidebar-link'>
+                        <i class="bi bi-bar-chart-fill"></i>
+                        <span>Hasil Perhitungan</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a href="logout.php" class='sidebar-link'>
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Logout</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+</div>
+
+
+<style>
+    .logo-sidebar {
+        text-align: center;
+    }
+</style>
